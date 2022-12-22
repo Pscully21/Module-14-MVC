3f1f6218583df4fc7a5b9148bb5e409fe58af5e1
Parker Scully <pscully05@gmail.com>
2022-12-22 18:48:39 -0500

    Added code to some routes, and an api folder

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const {
+    User,
+    Post,
+    Comment
+} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: {
+            exclude: ['password']
+        }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['pssword']
+        },
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+            },
+            {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: "There is no user with this ID"
+            });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
